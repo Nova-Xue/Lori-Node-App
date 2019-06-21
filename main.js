9d4841d85dbb35f3a3e5d453de556311c6c706bd
Nova <xueshengyuan1118@hotmail.com>
2019-06-21 14:22:18 -0400

    Test gitignore

---
+++ main.js
@@ -0,0 +1,22 @@
+var chalk = require("chalk");
+var axios = require("axios");
+var inquirer = require("inquirer");
+
+// function welcome(){
+//     console.log("Welcome");
+//     inquirer.prompt({
+//         type: 'confirm',
+//         name: 'test',
+//         message: 'Are you good?',
+//         default: true
+//     }).then((answer)=>{
+//         if(answer.test){
+//             welcome();
+//         }
+//     })
+// }
+// welcome();
+function welcome(){
+    console.log("Welome to Lori the Bot");
+    
+}
