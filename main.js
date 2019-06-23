1c27e7c603ed2de05bac894da436a6dbc647465c
Nova <xueshengyuan1118@hotmail.com>
2019-06-23 14:29:47 -0400

    Apply chalk to change command line colors and show keywords

--- main.js
+++ main.js
@@ -67,7 +67,7 @@ function searchSpotify(keyWord) {
 //write log when the search is done
 function checkCatalog(str1, str2) {
     if (str2 != "quit") {//quit when user inputs 'quit'
-        console.log("Lori is searching for " + str1);
+        console.log(chalk.blue.bold("Lori is searching for " + str1+"."));
         var query;
         switch (str2) {
             case "song":
@@ -96,7 +96,7 @@ function checkCatalog(str1, str2) {
                 break;
             case "file":
                 //read file function
-                console.log("Lori is reading the file");
+                console.log(chalk.blue.bold("Lori is reading the file."));
                 var fileCommand = readCommand(str1);
                 //checkCatalog(fileCommand.fileKey,fileCommand.fileCatalog);
                 if(checkCatalog(fileCommand.fileKey,fileCommand.fileCatalog)){
@@ -114,25 +114,26 @@ function checkCatalog(str1, str2) {
         }
 
     } else {
-        console.log("Thank you for using Lori the Bot!");
+        console.log(chalk.blue.bold("Thank you for using Lori the Bot!"));
     }
 }
 //main function 
+//
 function welcome() {
-    console.log("What can I do for you?");
-    console.log("You can quit the bot at anytime by typing in \'quit\'.");
+    console.log(chalk.blue.bold("What can I do for you?"));
+    console.log(chalk.blue.bold("You can quit the bot at anytime by typing in \'quit\'."));
     inquirer.prompt({//ask for key
         tyep: "input",
         name: "search",
-        message: "Search for something or read command from a file:",
-        default: "A song, a movie, your favorite band or a file name"
+        message: chalk.blue.bold("Search for something or read command from a file:"),
+        default: chalk.red.underline("A song, a movie, your favorite band or a file name")
     }).then((answer) => {
         if (answer.search != "quit") {
             let key = answer.search;
             inquirer.prompt({//ask for type//single choice
                 type: "expand",
                 name: "catalog",
-                message: "Is " + key + " a song, a movie, a band or a file? (You can type q to quit)",
+                message: chalk.blue("Is ")+ chalk.green(key) + chalk.blue(" a ")+chalk.yellow("song")+chalk.blue(", a ")+chalk.yellow("movie")+chalk.blue(", a ")+chalk.yellow("band")+chalk.blue(" or a ")+chalk.yellow("file")+chalk.blue("? ")+chalk.red.bold("(You can type q to quit,type h or enter for help)"),
                 choices: [
                     {
                         key: "s",
@@ -167,23 +168,23 @@ function welcome() {
                     inquirer.prompt({//ask user to continue or not
                         type: "confirm",
                         name: "again",
-                        message: "Do you need to search anything else?",
+                        message: chalk.blue.bold("Do you need to search anything else?"),
                         default: true
                     }).then((answer) => {
                         if (answer.again) {
                             welcome();//call main function to continue
                         } else {//quit
-                            console.log("Thank you for using Lori the Bot!");
+                            console.log(chalk.blue.bold("Thank you for using Lori the Bot!"));
                         }
                     });
                 }
             });
         } else {//quit
-            console.log("Thank you for using Lori the Bot!");
+            console.log(chalk.blue.bold("Thank you for using Lori the Bot!"));
         }
     });
 }
 //show once when enter the bot
-console.log("Welome to Lori the Bot!");
+console.log(chalk.blue.bold("Welome to Lori the Bot!"));
 //call main function
 welcome();
